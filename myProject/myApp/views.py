ee2aa6007d778ced569ad00ce58895f55073a816
Tamir <tamirphillips@live.com>
2025-02-15 22:57:47 -0500

    Added two pages

--- myProject/myApp/views.py
+++ myProject/myApp/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 def home(request):
     context = {}
     return render(request, 'myApp/home.html', context)
+
+def about(request):
+    context = {}
+    return render(request, 'myApp/about.html', context)
+
+def contact(request):
+    context = {}
+    return render(request, 'myApp/contact.html', context)
