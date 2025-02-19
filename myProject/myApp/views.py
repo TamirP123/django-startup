3b8f5993a52b19e0357efd1573ff65388f79633c
Tamir <tamirphillips@live.com>
2025-02-19 14:48:56 -0500

    Successfully grab contact information from form, redirect to homepage

--- myProject/myApp/views.py
+++ myProject/myApp/views.py
@@ -1,5 +1,6 @@
 from django_distill import distill_path
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect
 
 
 def home(request):
@@ -11,5 +12,17 @@ def about(request):
     return render(request, 'myApp/about.html', context)
 
 def contact(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        phone = request.POST.get("phone")
+
+        # Testing message
+        print(f"Received contact request from {name}, Email: {email}, Phone: {phone}")
+
+        messages.success(request, "Your message has been sent successfully!")
+        return redirect("home")  # Redirect to home page
+
     context = {}
+    
     return render(request, 'myApp/contact.html', context)
